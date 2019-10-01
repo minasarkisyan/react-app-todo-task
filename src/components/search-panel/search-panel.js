c25bab5238e0b73b4a24a2b9e433c73309b927d9
minas1986 <30533267+minas1986@users.noreply.github.com>
2019-10-01 14:21:40 +0300

    first commit

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchPanel = () => {
+
+    const searchText = 'Type here to search';
+
+    return <input className="foo" placeholder={searchText}/>
+}
+export default SearchPanel
